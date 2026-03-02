24d0288d5c6240cfbaa03618ca41cae7f3ccde8a
MattiaGalasso <galasso.mattia94@gmail.com>
2026-03-02 15:52:23 +0100

    initialize express server and set up posts router

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const appURL = `http://localhost:${port}`;
+
+// Imports
+const postsRouter = require("./routers/posts");
+
+// Middleware
+app.use(express.static("public"));
+
+// Routers
+app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+  console.log(`Server listenting on ${appURL}`);
+});
---
+++ routers/posts.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const posts = require("../data/posts");
+const router = express.Router();
